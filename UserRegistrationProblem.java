f7da188d21cb44d521d29ec01e4987173e3fe10e
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-08-03 17:29:56 +0530

    Has exactly 1 special character is solved.

--- UserRegistrationProblem.java
+++ UserRegistrationProblem.java
@@ -39,11 +39,11 @@ public class UserRegistrationProblem {
 		System.out.println(b);
 	}
 
-	//UC-7:- As a User need to follow predefined Password rules. Rule-3:- Should have at least 1 numeric number in the password
+	// UC-8:- As a User need to follow predefined Password rules. Rule-4:- Has exactly 1 Special Character.
 	public static void passwordValidation() {
 		System.out.println("Enter Password");
 		String password = input.nextLine();
-		boolean b = Pattern.matches("^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}$", password);
+		boolean b = Pattern.matches("^(?=.*[A-Z])(?=.*[0-9])(?=[\\w]*[\\W][\\w]*$)(?=.*[a-z]).{8,}$", password);
 		System.out.println(b);
 	}
 

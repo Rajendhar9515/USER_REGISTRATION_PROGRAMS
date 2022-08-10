2e1e3ad8facd441f85f32694bcb65b99e87bc88d
Rajendhar9515 <108645584+Rajendhar9515@users.noreply.github.com>
2022-08-10 20:42:58 +0530

    Update UserRegistrationTest.java

--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -9,70 +9,70 @@ public class UserRegistrationTest {
 	@Test
 	public void given_first_name_expected_as_true() {
 		UserRegistrationProblem uspTest = new UserRegistrationProblem();
-		boolean output = uspTest.firstNameValidation("Nikhil");
+		boolean output = uspTest.firstNameValidation("Rajendhar");
 		assertEquals(true , output);
 	}
 
 	@Test
 	public void given_first_name_expected_as_false() {
 		UserRegistrationProblem uspTest = new UserRegistrationProblem();
-		boolean output = uspTest.firstNameValidation("nikhil");
+		boolean output = uspTest.firstNameValidation("rajendhar");
 		assertEquals(false , output);
 	}
 	
 	@Test
 	public void given_last_name_expected_as_true() {
 		UserRegistrationProblem uspTest = new UserRegistrationProblem();
-		boolean output = uspTest.lastNameValidation("Sundarasetty");
+		boolean output = uspTest.lastNameValidation("Vemula");
 		assertEquals(true , output);
 	}
 	
 	@Test
 	public void given_last_name_expected_as_false() {
 		UserRegistrationProblem uspTest = new UserRegistrationProblem();
-		boolean output = uspTest.lastNameValidation("sundarasetty");
+		boolean output = uspTest.lastNameValidation("vemula");
 		assertEquals(false , output);
 	}
 	
 	@Test
 	public void given_email_expected_as_true() {
 		UserRegistrationProblem uspTest = new UserRegistrationProblem();
-		boolean output = uspTest.emailValidation("nikhilnaidu473@gmail.com");
+		boolean output = uspTest.emailValidation("vemulrajendhar1@gmail.com");
 		assertEquals(true , output);
 	}
 	
 	@Test
 	public void given_email_expected_as_false() {
 		UserRegistrationProblem uspTest = new UserRegistrationProblem();
-		boolean output = uspTest.emailValidation("nikhilnaidu473@.gmail.com");
+		boolean output = uspTest.emailValidation("rajendhar@.gmail.com");
 		assertEquals(false , output);
 	}
 	
 	@Test
 	public void given_mobile_number_expected_as_true() {
 		UserRegistrationProblem uspTest = new UserRegistrationProblem();
-		boolean output = uspTest.mobileNumberValidation("91 9398359637");
+		boolean output = uspTest.mobileNumberValidation("91 4532324523");
 		assertEquals(true , output);
 	}
 	
 	@Test
 	public void given_mobile_number_expected_as_false() {
 		UserRegistrationProblem uspTest = new UserRegistrationProblem();
-		boolean output = uspTest.mobileNumberValidation("919398359637");
+		boolean output = uspTest.mobileNumberValidation("914532324523");
 		assertEquals(false , output);
 	}
 	
 	@Test
 	public void given_password_expected_as_true() {
 		UserRegistrationProblem uspTest = new UserRegistrationProblem();
-		boolean output = uspTest.passwordValidation("Nikhil123@");
+		boolean output = uspTest.passwordValidation("Rajendhar12@");
 		assertEquals(true , output);
 	}
 	
 	@Test
 	public void given_password_expected_as_false() {
 		UserRegistrationProblem uspTest = new UserRegistrationProblem();
-		boolean output = uspTest.passwordValidation("Nikhi$l123@");
+		boolean output = uspTest.passwordValidation("Raja$l123@");
 		assertEquals(false , output);
 	}
 }
